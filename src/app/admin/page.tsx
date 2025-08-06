4ddfd87cf0c0f0624aa14a8ad6ab0cc32dc7bc86
Dimas <dimas@kemaspkg.com>
2025-08-06 04:45:29 +0000

    hapus text judul Upcoming Scheduled Content Here is a list of all your s

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -62,12 +62,7 @@ export default function AdminPage() {
         </div>
       </div>
        <div className="mt-12 flex-1 flex flex-col">
-        <div className="mb-8">
-            <h2 className="text-2xl font-bold tracking-tight">Upcoming Scheduled Content</h2>
-            <p className="text-muted-foreground">
-                Here is a list of all your scheduled content.
-            </p>
-        </div>
+        
         {schedules.length > 0 ? (
            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
             {schedules.map((schedule) => (
